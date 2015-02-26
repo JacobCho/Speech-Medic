73ff5378cc012e5133236e17367b8c3e0b2360bd
Jacob Cho <jacobshcho@gmail.com>
2015-02-26 11:44:37 -0800

    Set up tableview and adding new patients

---
+++ Speech Medic/PatientsViewController.swift
@@ -0,0 +1,121 @@
+//
+//  PatientsViewController.swift
+//  Speech Medic
+//
+//  Created by Jacob Cho on 2015-02-26.
+//  Copyright (c) 2015 Jacob. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class PatientsViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var patients = [NSManagedObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.fetchPatients()
+        
+    }
+    
+    @IBAction func addPatient(sender: UIBarButtonItem) {
+        var alert = UIAlertController(title: "New Patient",
+            message: "Please enter the patient's name",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let textField = alert.textFields![0] as UITextField
+                self.saveName(textField.text)
+                self.tableView.reloadData()
+        }
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return patients.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("PatientCell")
+                as UITableViewCell
+            
+            let patient = patients[indexPath.row]
+            cell.textLabel!.text = patient.valueForKey("name") as String?
+            return cell
+    }
+    
+    func saveName(name: String) {
+
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext!
+        
+        let entity =  NSEntityDescription.entityForName("Patient",
+            inManagedObjectContext:
+            managedContext)
+        
+        let patient = NSManagedObject(entity: entity!,
+            insertIntoManagedObjectContext:managedContext)
+        
+        patient.setValue(name, forKey: "name")
+        
+        var error: NSError?
+        if !managedContext.save(&error) {
+            println("Could not save \(error), \(error?.userInfo)")
+        }
+
+        patients.append(patient)
+    }
+    
+    func fetchPatients() {
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext!
+        
+        let fetchRequest = NSFetchRequest(entityName:"Patient")
+        
+        var error: NSError?
+        
+        let fetchedResults =
+        managedContext.executeFetchRequest(fetchRequest,
+            error: &error) as [NSManagedObject]?
+        
+        if let results = fetchedResults {
+            patients = results
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+        }
+    }
+    
+
+}
