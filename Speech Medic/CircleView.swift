6f72e953b725fb181ed3106dfab080203afde6e5
Jacob Cho <jacobshcho@gmail.com>
2015-03-06 15:33:03 -0800

    Added Circle View to Diagnosis table view cell

---
+++ Speech Medic/CircleView.swift
@@ -0,0 +1,13 @@
+//
+//  CircleView.swift
+//  Speech Medic
+//
+//  Created by Jacob Cho on 2015-03-06.
+//  Copyright (c) 2015 Jacob. All rights reserved.
+//
+
+import Cocoa
+
+class CircleView: UIView {
+
+}
